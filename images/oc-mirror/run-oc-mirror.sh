98fad221b8d9292ed8f1ad8e35dff6192af10048
Josh Swanson <jjaswanson4@gmail.com>
2024-09-17 12:28:42 -0500

    update script

--- images/oc-mirror/run-oc-mirror.sh
+++ images/oc-mirror/run-oc-mirror.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Part 1 - Get oc-mirror command
-curl https://developers.redhat.com/content-gateway/rest/mirror/pub/openshift-v4/clients/mirror-registry/latest/mirror-registry.tar.gz -o oc-mirror.tar.gz
+curl https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/latest/oc-mirror.tar.gz -o oc-mirror.tar.gz
 tar -xzvf oc-mirror.tar.gz
 chmod +x oc-mirror
 
+# Part 2 - Decode base64 pull secret if needed
+
 # Part 2 - Run mirror
-./oc-mirror --config=/tmp/imagesetconfiguration.yaml docker://registry."$NAMESPACE".svc.cluster.local:5000 --dest-use-http=true
+./oc-mirror --config=/tmp/imagesetconfiguration.yaml docker://"$REGISTRY" --dest-use-http=$DEST_USE_HTTP --dest-skip-tls=$DEST_SKIP_TLS
